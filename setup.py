cbac04cee6d3dd3a1ebd3d4733f129f24af04f21
Mark Roddy <markroddy@gmail.com>
2016-07-19 00:48:53 +0000

    setup django dependency

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(name='dtrender',
-            version='0.1',
-            description='Command Line Rendering of Django Templates',
-            author='Mark Roddy',
-            author_email='markroddy@gmail.com',
-            url='http://bitbucket.org/markroddy/dtrender',
-            py_modules=['dtrender',],
-            scripts=['dtrender',],
-            install_requires=["Django==1.2.7"]
-                 )
+      version='0.1',
+      description='Command Line Rendering of Django Templates',
+      author='Mark Roddy',
+      author_email='markroddy@gmail.com',
+      url='http://bitbucket.org/markroddy/dtrender',
+      py_modules=['dtrender',],
+      scripts=['dtrender',],
+      install_requires=["Django==1.2.7"]
+           )
