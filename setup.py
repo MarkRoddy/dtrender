788ed719288e7e5a44652ea392fb742428e95bc5
Mark Roddy <markroddy@gmail.com>
2016-07-19 00:48:03 +0000

    ignore dist generated files

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from distutils.core import setup
 
 setup(name='dtrender',
-      version='0.1',
-      description='Command Line Rendering of Django Templates',
-      author='Mark Roddy',
-      author_email='markroddy@gmail.com',
-      url='http://bitbucket.org/markroddy/dtrender',
-      py_modules=['dtrender',],
-      scripts=['dtrender',]
-           )
+            version='0.1',
+            description='Command Line Rendering of Django Templates',
+            author='Mark Roddy',
+            author_email='markroddy@gmail.com',
+            url='http://bitbucket.org/markroddy/dtrender',
+            py_modules=['dtrender',],
+            scripts=['dtrender',],
+            install_requires=["Django==1.2.7"]
+                 )
